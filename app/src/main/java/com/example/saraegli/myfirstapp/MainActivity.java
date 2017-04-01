45441a54d006803a4aca04cdc507481bdb6db183
saraegli <5myrorEfler>
2017-04-01 13:22:39 +0200

    Added Activity to show sensor values.

--- app/src/main/java/com/example/saraegli/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/saraegli/myfirstapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.saraegli.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the "Compass"-button */
+    public void openCompass(View view) {
+        Intent intent = new Intent(this, Compass.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the "Values"-button */
+    public void openValues(View view) {
+        Intent intent = new Intent(this, Values.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/saraegli/myfirstapp/Values.java
@@ -0,0 +1,16 @@
+package com.example.saraegli.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by saraegli on 2017-04-01.
+ */
+
+public class Values extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_values);
+    }
+}
